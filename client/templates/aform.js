49c3bb0bd3f1c807000cfb9344b4c2f638aab46c
Cosmin <cosminmldn@gmail.com>
2016-06-26 16:46:55 +0300

    Config Answer Input data

--- client/templates/aform.js
+++ client/templates/aform.js
@@ -1,5 +1,5 @@
 /**Template.afrom.created = function() {
-  Meteor.Loader.loadCss("../sylesheets/aform.css");
+Meteor.Loader.loadCss("../sylesheets/aform.css");
 };
 */
 Answers = new Mongo.Collection('answers');
@@ -8,32 +8,51 @@ var Users = function () {
   return users.find({}).fetch({});
 }
 Template.aform.helpers({
-questions: function () {
-  return Questions.find({}).fetch({});
-},
-/*users: function () {
+  questions: function () {
+    return Questions.find({}).fetch({});
+  },
+  /*users: function () {
   return Users.find({}).fetch({});
 }*/
 
 });
 
 Template.aform.events({
-  "click #a": function () {
-    fag = Questions.find().fetch();
-    console.log(fag);
+  "click #close": function (e) {
+    event.preventDefault();
+    console.log("nimic!")
+    $( e.currentTarget ).parent().parent().slideUp();
   },
   "click .card-action": function(e){
-    /*Answers.insert({
-      userId: $("[name= db.getCollection('users').find({_id})]").val(),
-      Answer: $("[name='answer']").val(),
-    });*/
-    $( e.currentTarget ).parent().parent().parent().parent().slideUp();
+    //event.preventDefault();
+    Answers.insert({
+      userId: Meteor.userId(),
+      Answer: $("#"+e.currentTarget.attributes.data.value).val(),
+      QuestionID: e.currentTarget.attributes.data.value,
+      DateAns : new Date(),
+      AnsValid : false,
+    });
+    //validate
+    var rasp = $("#"+e.currentTarget.attributes.data.value).val().toLowerCase();
+    var raspC = Questions.findOne(e.currentTarget.attributes.data.value).option1.toLowerCase() ;
+  
+    if (rasp == raspC) {
+      //If answer matches
+      console.log("Raspunsuri",rasp,raspC);
+      $( e.currentTarget).parent().addClass("btn-success");
+    }
+    else {
+      console.log("gresit!!");
+      $( e.currentTarget ).parent().addClass("btn-warning");
+    }
+    //$( e.currentTarget ).parent().parent().parent().parent().slideUp();
     console.log(e.currentTarget.attributes.data.value); //questionID
     console.log(Meteor.userId());
-    console.log($("#"+e.currentTarget.attributes.data.value).val())
-    console.log(Questions.findOne(e.currentTarget.attributes.data.value).option1)
+    console.log($("#"+e.currentTarget.attributes.data.value).val()) //Input Answer
+    console.log(Questions.findOne(e.currentTarget.attributes.data.value).option1)//Answer from Qform
+    console.log("ceva ",Answers.find().fetch())
 
-  //  console.log(db.getCollection('users').find({_id}));
+    //  console.log(db.getCollection('users').find({_id}));
 
   }
 
--- server/Trivia.js
+++ server/Trivia.js
@@ -1,12 +1,13 @@
 if (Meteor.isServer) {
-    
-    
+
+
   Meteor.startup(function () {
    Categories = new Mongo.Collection('categories');
    Questions = new Mongo.Collection('questions');
+   Answers = new Mongo.Collection('answers');
      console.log("server going");
-    
+
   });
-  
-  
+
+
 }
