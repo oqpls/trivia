dedf986f7d9d8c8b514605bca1c434a3fc228703
Razvan Ganea <rganea@pentalog.fr>
2016-06-26 14:45:26 +0300

    Small changes

--- client/templates/board.js
+++ client/templates/board.js
@@ -4,13 +4,12 @@ Template.board.helpers({
     users: function () {
       return Users.find({}).fetch({});
     },
-    test : "potato"
 
 });
 
 
 Template.board.events({
-    'click .row': function(event){
+    'click .clickrow': function(event){
   //to do: actually make the form save data.
      event.preventDefault();
     console.log(Meteor.userId());
