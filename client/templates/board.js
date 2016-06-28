9fab9d9504c992341e43023d281db7a26c124fd3
Cosmin <cosminmldn@gmail.com>
2016-06-28 12:38:28 +0300

    Minor Text changes

--- client/templates/board.js
+++ client/templates/board.js
@@ -7,13 +7,11 @@ Template.board.helpers({
     },
 });
 
-
 Template.board.events({
     'click .clickrow': function(event){
   //to do: actually make the form save data.
-     event.preventDefault();
+
   console.log(Meteor.users.find({},{fields: {'services':0}} ).fetch());
-  
 },
 "click .delete": function (e) {
     Meteor.users.remove(e.currentTarget.attributes.data.value);
