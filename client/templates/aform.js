f57c5d74224828bbd7eafb0fd0f3ae0e43e79907
Cosmin <cosminmldn@gmail.com>
2016-07-07 00:08:27 +0300

    Fixed scoring system on answer list

--- client/templates/aform.js
+++ client/templates/aform.js
@@ -22,34 +22,43 @@ Template.aform.events({
     $( e.currentTarget ).parent().parent().slideUp();
   },
   "click .card-action": function(e){
-    //event.preventDefault();
-    Answers.insert({
-      userId: Meteor.userId(),
-      Answer: $("#"+e.currentTarget.attributes.data.value).val(),
-      QuestionID: e.currentTarget.attributes.data.value,
-      DateAns : new Date(),
-      AnsValid : false,
-    });
+
     //validate
     var rasp = $("#"+e.currentTarget.attributes.data.value).val().toLowerCase();
     var raspC = Questions.findOne(e.currentTarget.attributes.data.value).option1.toLowerCase() ;
+    var newScore = 0;
 
+    var compare = function() {
+      $( e.currentTarget).addClass("hidden");
     if (rasp == raspC) {
       //If answer matches
       console.log("Raspunsuri",rasp,raspC);
       $( e.currentTarget).parent().addClass("btn-success");
+      newScore = 15;
+      console.log(newScore);
+      return true
     }
     else {
       console.log("gresit!!");
       $( e.currentTarget ).parent().addClass("btn-warning");
-    }
+      return false
+    }}
     //$( e.currentTarget ).parent().parent().parent().parent().slideUp();
     console.log(e.currentTarget.attributes.data.value); //questionID
     console.log(Meteor.userId());
     console.log($("#"+e.currentTarget.attributes.data.value).val()) //Input Answer
     console.log(Questions.findOne(e.currentTarget.attributes.data.value).option1)//Answer from Qform
     console.log("ceva ",Answers.find().fetch())
+    console.log(newScore);
 
+    Answers.insert({
+      userId: Meteor.userId(),
+      Answer: $("#"+e.currentTarget.attributes.data.value).val(),
+      QuestionID: e.currentTarget.attributes.data.value,
+      DateAns : new Date(),
+      AnsValid : compare(),
+      Score : newScore,
+    });
     //  console.log(db.getCollection('users').find({_id}));
 
   }
